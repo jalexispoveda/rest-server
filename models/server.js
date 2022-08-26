3298bab5a7d1c9fe40feb56484b05623a8e19bec
Jorge Poveda <JPoveda@redlobster.com>
2022-08-26 06:03:47 -0500

    Created HTTP verbs

--- models/server.js
+++ models/server.js
@@ -1,31 +1,51 @@
 const express = require("express");
 class Server {
-  constructor() {
-    //Cuando instancie esta clase creare el nuevo servidor
-    this.app = express();
-    this.port = process.env.PORT;
-    //Middlewares (funcion que siempre se ejecuta al levantar el servidor)
-    this.middlewares();
-    //Esto dispara el metodo que configura mis rutas
-    this.routes();
-  }
+	constructor() {
+		//Cuando instancie esta clase creare el nuevo servidor
+		this.app = express();
+		this.port = process.env.PORT;
+		//Middlewares (funcion que siempre se ejecuta al levantar el servidor)
+		this.middlewares();
+		//Esto dispara el metodo que configura mis rutas
+		this.routes();
+	}
 
-  middlewares() {
-    //directorio publico (front)
-    this.app.use(express.static("public"));
-  }
+	middlewares() {
+		//directorio publico (front)
+		this.app.use(express.static("public"));
+	}
 
-  routes() {
-    this.app.get("/", (req, res) => {
-      res.send("Hello World");
-    });
-  }
+	routes() {
+		this.app.get("/api", (req, res) => {
+			res.json({
+				msg: "get API",
+			});
+		});
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log("Servidor corriendo en puerto ", this.port);
-    });
-  }
+		this.app.put("/api", (req, res) => {
+			res.json({
+				msg: "put API",
+			});
+		});
+
+		this.app.post("/api", (req, res) => {
+			res.json({
+				msg: "post API",
+			});
+		});
+
+		this.app.delete("/api", (req, res) => {
+			res.json({
+				msg: "delete API",
+			});
+		});
+	}
+
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log("Servidor corriendo en puerto ", this.port);
+		});
+	}
 }
 
 module.exports = Server;
