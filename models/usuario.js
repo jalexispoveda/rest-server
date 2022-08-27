0c1c2e25dec130e71d6df2daa5cd98af531f6d24
jalexispoveda010 <jalexispoveda010@gmail.com>
2022-08-27 16:11:22 -0500

    Esquema usuario creado

---
+++ models/usuario.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const usuarioSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  correo: {
+    type: String,
+    required: [true, "El correo es obligatorio"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "El password es obligatorio"],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+//De esta manera se exporta el esquema para que
+//se llame usuarios la tabla
+module.exports = model("Usuarios", usuarioSchema);
