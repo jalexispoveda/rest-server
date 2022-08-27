4e2698f37c52591f433b29201c2ae2807f3ee61c
jalexispoveda010 <jalexispoveda010@gmail.com>
2022-08-27 06:27:01 -0500

    Getting params from routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,48 @@
-const { response } = require("express");
+const { response, request } = require("express");
 
-const getUsuarios = (req, res = response) => {
-	res.json({
-		msg: "get API controller",
-	});
+const getUsuarios = (req = request, res = response) => {
+  const queryParams = req.query;
+
+  res.json({
+    msg: "get API controller",
+    queryParams,
+  });
 };
 
 const postUsuarios = (req, res = response) => {
-	console.log(req);
+  console.log(req);
 
-	const body = req.body;
-	res.json({
-		msg: "post API controller",
-		body,
-	});
+  const body = req.body;
+  res.json({
+    msg: "post API controller",
+    body,
+  });
 };
 
 const putUsuarios = (req, res = response) => {
-	res.json({
-		msg: "put API controller",
-	});
+  const { id } = req.params;
+  res.json({
+    msg: "put API controller",
+    id,
+  });
 };
 
 const patchUsuarios = (req, res = response) => {
-	res.json({
-		msg: "patch API controller",
-	});
+  res.json({
+    msg: "patch API controller",
+  });
 };
 
 const deleteUsuarios = (req, res = response) => {
-	res.json({
-		msg: "delete API controller",
-	});
+  res.json({
+    msg: "delete API controller",
+  });
 };
 
 module.exports = {
-	getUsuarios,
-	postUsuarios,
-	putUsuarios,
-	patchUsuarios,
-	deleteUsuarios,
+  getUsuarios,
+  postUsuarios,
+  putUsuarios,
+  patchUsuarios,
+  deleteUsuarios,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
 const {
-	getUsuarios,
-	putUsuarios,
-	postUsuarios,
-	deleteUsuarios,
-	patchUsuarios,
+  getUsuarios,
+  putUsuarios,
+  postUsuarios,
+  deleteUsuarios,
+  patchUsuarios,
 } = require("../controllers/user");
 
 const router = Router();
 
 router.get("/", getUsuarios);
 
-router.put("/", putUsuarios);
+router.put("/:id", putUsuarios);
 
 router.post("/", postUsuarios);
 
