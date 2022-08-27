e71ba86e3c3a4283c044023b24e8127c07ed9e90
jalexispoveda010 <jalexispoveda010@gmail.com>
2022-08-27 15:53:37 -0500

    Connected with mongo DB using mongoose

--- models/server.js
+++ models/server.js
@@ -1,35 +1,42 @@
 var cors = require("cors");
 const express = require("express");
+const { dbConnection } = require("../database/config");
 class Server {
-	constructor() {
-		//Cuando instancie esta clase creare el nuevo servidor
-		this.app = express();
-		this.port = process.env.PORT;
-		this.usuariosPath = "/api/usuarios";
-		//Middlewares (funcion que siempre se ejecuta al levantar el servidor)
-		this.middlewares();
-		//Esto dispara el metodo que configura mis rutas
-		this.routes();
-	}
+  constructor() {
+    //Cuando instancie esta clase creare el nuevo servidor
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
+    //Conectar a BD
+    this.conectarDb();
+    //Middlewares (funcion que siempre se ejecuta al levantar el servidor)
+    this.middlewares();
+    //Esto dispara el metodo que configura mis rutas
+    this.routes();
+  }
 
-	middlewares() {
-		//CORS
-		this.app.use(cors());
-		//lectura y parseo de body
-		this.app.use(express.json());
-		//directorio publico (front)
-		this.app.use(express.static("public"));
-	}
+  async conectarDb() {
+    await dbConnection();
+  }
 
-	routes() {
-		this.app.use(this.usuariosPath, require("../routes/user"));
-	}
+  middlewares() {
+    //CORS
+    this.app.use(cors());
+    //lectura y parseo de body
+    this.app.use(express.json());
+    //directorio publico (front)
+    this.app.use(express.static("public"));
+  }
 
-	listen() {
-		this.app.listen(this.port, () => {
-			console.log("Servidor corriendo en puerto ", this.port);
-		});
-	}
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/user"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto ", this.port);
+    });
+  }
 }
 
 module.exports = Server;
