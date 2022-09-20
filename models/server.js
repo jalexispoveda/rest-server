b655cbba41cf43a120dffe45beeb0dda03f251cd
Jorge Poveda <JPoveda@redlobster.com>
2022-09-20 05:51:57 -0500

    changed port to optional

--- models/server.js
+++ models/server.js
@@ -2,41 +2,41 @@ var cors = require("cors");
 const express = require("express");
 const { dbConnection } = require("../database/config");
 class Server {
-  constructor() {
-    //Cuando instancie esta clase creare el nuevo servidor
-    this.app = express();
-    this.port = process.env.PORT;
-    this.usuariosPath = "/api/usuarios";
-    //Conectar a BD
-    this.conectarDb();
-    //Middlewares (funcion que siempre se ejecuta al levantar el servidor)
-    this.middlewares();
-    //Esto dispara el metodo que configura mis rutas
-    this.routes();
-  }
+	constructor() {
+		//Cuando instancie esta clase creare el nuevo servidor
+		this.app = express();
+		this.port = process.env.PORT || 3000;
+		this.usuariosPath = "/api/usuarios";
+		//Conectar a BD
+		this.conectarDb();
+		//Middlewares (funcion que siempre se ejecuta al levantar el servidor)
+		this.middlewares();
+		//Esto dispara el metodo que configura mis rutas
+		this.routes();
+	}
 
-  async conectarDb() {
-    await dbConnection();
-  }
+	async conectarDb() {
+		await dbConnection();
+	}
 
-  middlewares() {
-    //CORS
-    this.app.use(cors());
-    //lectura y parseo de body
-    this.app.use(express.json());
-    //directorio publico (front)
-    this.app.use(express.static("public"));
-  }
+	middlewares() {
+		//CORS
+		this.app.use(cors());
+		//lectura y parseo de body
+		this.app.use(express.json());
+		//directorio publico (front)
+		this.app.use(express.static("public"));
+	}
 
-  routes() {
-    this.app.use(this.usuariosPath, require("../routes/user"));
-  }
+	routes() {
+		this.app.use(this.usuariosPath, require("../routes/user"));
+	}
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log("Servidor corriendo en puerto ", this.port);
-    });
-  }
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log("Servidor corriendo en puerto ", this.port);
+		});
+	}
 }
 
 module.exports = Server;
